e560b590507ed4d31697a76fb0690a2c54e78658
Apple <Apple@AppledeMacBook-Pro.local>
2017-11-02 21:08:32 -0700

    feature1,feature2 & feature3

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # InteractiveSnowBalls
 A "Network" of Snow balls that can response to the mouse.
+I am new to Javascript, but I have some experience coding in python and I have basic knowledge of data structure and algorithm.
+
+## The first day I want to add nodes and edges
+
+###Feature 1 - Build a class called Jitter
+
+It is not quite the same build a class. In fact, Javascript does not have class. Everything is an instance.
+1.1 Jitter should have a location (x,y).
+1.2 Jitter should use a circle to display itself.
+
+###Feature 2 - Build a class to represent the edges of each Jitter
+2.1 It takes 2 parameters: pointA and pointB, which are 2 Jitter objects.
+2.2 It needs a way to display itself. I use p5.js.line.
+2.3 I define the edge will display only when the two points are within a certain distance.
+
+###Feature 3 - Connects the dots.
+Use 2 for loops to check every Jitter's distance and check whether to display the edge or not. This algorithm takes O(n^2) of time.
--- sketch.js
+++ sketch.js
@@ -1,29 +1,41 @@
 var bugs = [];
-
+    edges = [];
+    numOfNode = 150;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  noStroke();
+
   // Create object
-  for (i = 0; i < 40; i++) {
+  for (i = 0; i < numOfNode; i++) {
     var bug = new Jitter();
     bugs.push(bug);
   };
+  for (i = 0; i < numOfNode-1; i++) {
+    for (j = 1; j<numOfNode; j++){
+      var pointA = bugs[i];
+      var pointB = bugs[j];
+      var edge = new Edge(pointA, pointB)
+      edges.push(edge)
+    }
+  }
 }
 
 function draw() {
   background(50, 89, 100);
-  for (i = 0; i < 40; i++) {
+  for (i = 0; i < bugs.length; i++) {
     bugs[i].move();
     bugs[i].display();
   };
+  for (i = 0; i < edges.length; i++) {
+    edges[i].display();
+  }
 }
 
 // Node class
 function Jitter() {
   this.x = random(width);
   this.y = random(height);
-  this.diameter = random(10, 30);
+  this.diameter = random(10,15);
   this.speed = 2;
 
   this.move = function() {
@@ -35,3 +47,16 @@ function Jitter() {
     ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 };
+
+//Edge class
+function Edge(pointA, pointB) {
+  this.a = pointA;
+  this.b = pointB;
+  this.display = function (){if(dist(this.a.x, this.a.y, this.b.x, this.b.y) < 150){
+    line(this.a.x, this.a.y, this.b.x, this.b.y)
+    stroke(255,255,255)
+    }
+
+  }
+
+}
