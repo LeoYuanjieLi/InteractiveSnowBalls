b483844b4a057b2b71417837addf682a7bd803f2
Apple <Apple@AppledeMacBook-Pro.local>
2017-11-04 11:51:28 -0700

    feature 4 & 5

--- README.md
+++ README.md
@@ -11,9 +11,19 @@ It is not quite the same build a class. In fact, Javascript does not have class.
 1.2 Jitter should use a circle to display itself.
 
 ### Feature 2 - Build a class to represent the edges of each Jitter
+
 2.1 It takes 2 parameters: pointA and pointB, which are 2 Jitter objects.
 2.2 It needs a way to display itself. I use p5.js.line.
 2.3 I define the edge will display only when the two points are within a certain distance.
 
 ### Feature 3 - Connects the dots.
+
 Use 2 for loops to check every Jitter's distance and check whether to display the edge or not. This algorithm takes O(n^2) of time.
+
+### Feature 4 - Mouse node.
+
+4.1 Create a node when you move your mouse, and connect to the "local" nodes within a certain distance.
+
+### Feature 5 - Moving nodes.
+
+5.1 Create moving modes that connects to the local (but not each other! It will be too many lines!)
--- sketch.js
+++ sketch.js
@@ -1,23 +1,36 @@
 var bugs = [];
     edges = [];
-    numOfNode = 150;
+    numOfNode = 50;
+var movingBugs = [];
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 
-  // Create object
+  // Create nodes
   for (i = 0; i < numOfNode; i++) {
     var bug = new Jitter();
     bugs.push(bug);
   };
+  // Creating Edges
   for (i = 0; i < numOfNode-1; i++) {
     for (j = 1; j<numOfNode; j++){
       var pointA = bugs[i];
       var pointB = bugs[j];
       var edge = new Edge(pointA, pointB)
       edges.push(edge)
-    }
+    };
+  };
+
+  // Creating moving nodes
+  for (i = 0; i < 70; i++){
+        x = random(width);
+        y = random(height);
+        speedX = random(0,5);
+        speedY = random(3,7);
+        var movingBug = new movingJitter(x,y,speedX, speedY);
+        movingBugs.push(movingBug);
   }
+
 }
 
 function draw() {
@@ -28,19 +41,59 @@ function draw() {
   };
   for (i = 0; i < edges.length; i++) {
     edges[i].display();
-  }
-}
+  };
+  ellipse(mouseX, mouseY, 20, 20);
+
+  for (i = 0; i < bugs.length; i++) {
+    if (dist(mouseX, mouseY, bugs[i].x, bugs[i].y)<150){
+      line(mouseX, mouseY,bugs[i].x, bugs[i].y);
+    };
+  };
+
+  for (i = 0; i < movingBugs.length; i++){
+    movingBugs[i].move();
+    movingBugs[i].display();
+    for (j = 0; j < bugs.length; j++){
+      if (dist(movingBugs[i].x, movingBugs[i].y, bugs[j].x, bugs[j].y)<150){
+        line(movingBugs[i].x, movingBugs[i].y, bugs[j].x, bugs[j].y);
+      };
+    };
+  };
+};
 
 // Node class
 function Jitter() {
   this.x = random(width);
   this.y = random(height);
-  this.diameter = random(10,15);
+  this.diameter = random(20,25);
   this.speed = 2;
 
   this.move = function() {
     this.x += random(-this.speed, this.speed);
     this.y += random(-this.speed, this.speed);
+  };
+
+  this.display = function() {
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+};
+//Moving Jitters
+function movingJitter(x,y,speedX, speedY) {
+  this.x = x;
+  this.y = y;
+  this.diameter = random(10,15);
+  this.speedX = speedX;
+  this.speedY = speedY;
+
+  this.move = function() {
+    this.x = this.x + this.speedX;
+    this.y = this.y - this.speedY;
+    if (this.y < 0 || this. y > height) {
+      this.speedY = -this.speedY;
+    }
+    else if (this.x < 0 || this. x > width) {
+      this.speedX = -this.speedX;
+    };
   };
 
   this.display = function() {
@@ -59,4 +112,4 @@ function Edge(pointA, pointB) {
 
   }
 
-}
+};
